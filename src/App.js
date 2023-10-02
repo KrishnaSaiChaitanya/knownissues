800c10da717f67c566d3b44590bdc10f319c3600
chaitanya <chaitanya.tata215@gmail.com>
2023-10-03 01:38:34 +0530

    Updated component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import TextSlider from "./Components/TextSlider";
+import Home from "./Components/Home";
 
 function App() {
   const texts = [
---
+++ src/Components/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// import "./Home.css";
+function Home() {
+  return (
+    <div>
+      <div
+        className=""
+        style={{ backgroundColor: "white", height: "40vh" }}
+      ></div>
+      <div class="divider divider-alt text-light mx-n2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100">
+          <g fill="white">
+            <polygon points="1440 100 0 15 0 0 1440 0 1440 100"></polygon>
+          </g>
+        </svg>
+        {/* <img src="./bg-wave.png" style={{ width: "100%" }} /> */}
+      </div>
+
+      {/* <h1>Do or do not there is no try</h1> */}
+
+      {/* <img src="devices.png" style={{ height: "200px" }} /> */}
+    </div>
+  );
+}
+
+export default Home;
